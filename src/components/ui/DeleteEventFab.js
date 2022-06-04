f6a67fbfbe3981039bd3add5776ecc4536f19684
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-06-03 21:46:04 -0300

    Code formatted in DeleteEventFab.js

--- src/components/ui/DeleteEventFab.js
+++ src/components/ui/DeleteEventFab.js
@@ -1,20 +1,17 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
-import { eventStartDelete } from '../../actions/events';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { eventStartDelete } from "../../actions/events";
 
 export const DeleteEventFab = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleDelete = ()=>{
-        dispatch(eventStartDelete());
-    }
+  const handleDelete = () => {
+    dispatch(eventStartDelete());
+  };
   return (
-    <button
-        className='btn btn-danger fab-danger'
-        onClick={handleDelete}
-    >
-        <i className='fas fa-trash'></i>
-        <span> Borrar evento</span>
+    <button className="btn btn-danger fab-danger" onClick={handleDelete}>
+      <i className="fas fa-trash"></i>
+      <span> Borrar evento</span>
     </button>
-  )
-}
+  );
+};
