ce7c7bbd4f76b00420dcedf37f7ecede04fd67a2
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2022-06-04 17:21:02 -0300

    New unit tests added.

--- src/tests/router/AppRouter.test.js
+++ src/tests/router/AppRouter.test.js
@@ -1,74 +1,74 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
+import React from "react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 import thunk from "redux-thunk";
-import '@testing-library/jest-dom';
-import { mount } from 'enzyme';
-import {AppRouter} from '../../router/AppRouter';
+import "@testing-library/jest-dom";
+import { mount } from "enzyme";
+import { AppRouter } from "../../router/AppRouter";
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
 // store.dispatch = jest.fn();
 
-describe('Pruebas en AppRouter', () => {
-    test('debe de mostrar el "espere..." ', () => {
-        const initState={
-            auth: {
-                checking: true
-            }
-        };
-        const store = mockStore(initState);
+describe("Pruebas en AppRouter", () => {
+  test('debe de mostrar el "espere..." ', () => {
+    const initState = {
+      auth: {
+        checking: true,
+      },
+    };
+    const store = mockStore(initState);
 
-        const wrapper = mount(
-            <Provider store={store}>
-                <AppRouter></AppRouter>
-            </Provider>
-        );
-        expect(wrapper).toMatchSnapshot();
-        expect(wrapper.find('h5').exists).toBe(true);
-    });
+    const wrapper = mount(
+      <Provider store={store}>
+        <AppRouter></AppRouter>
+      </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find("h5").exists()).toBe(true);
+  });
 
-    test('debe de mostrar la ruta pública ', () => {
-        const initState={
-            auth: {
-                checking: false,
-                uid: null
-            }
-        };
-        const store = mockStore(initState);
+  test("debe de mostrar la ruta pública ", () => {
+    const initState = {
+      auth: {
+        checking: false,
+        uid: null,
+      },
+    };
+    const store = mockStore(initState);
 
-        const wrapper = mount(
-            <Provider store={store}>
-                <AppRouter></AppRouter>
-            </Provider>
-        );
-        expect(wrapper).toMatchSnapshot();
-        expect(wrapper.find('.login-container').exists).toBe(true);
-    });
+    const wrapper = mount(
+      <Provider store={store}>
+        <AppRouter></AppRouter>
+      </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find(".login-container").exists()).toBe(true);
+  });
 
-    test('debe de mostrar la ruta privada ', () => {
-        const initState={
-            calendar: {
-                events: []
-            },
-            ui: {
-                modalOpen: false
-            },
-            auth: {
-                checking: false,
-                uid: '123',
-                name: 'Juan Carlos'
-            }
-        };
-        const store = mockStore(initState);
+  test("debe de mostrar la ruta privada ", () => {
+    const initState = {
+      calendar: {
+        events: [],
+      },
+      ui: {
+        modalOpen: false,
+      },
+      auth: {
+        checking: false,
+        uid: "123",
+        name: "Juan Carlos",
+      },
+    };
+    const store = mockStore(initState);
 
-        const wrapper = mount(
-            <Provider store={store}>
-                <AppRouter></AppRouter>
-            </Provider>
-        );
-        expect(wrapper).toMatchSnapshot();
-        expect(wrapper.find('.calendar-screen').exists).toBe(true);
-    });
+    const wrapper = mount(
+      <Provider store={store}>
+        <AppRouter></AppRouter>
+      </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find(".calendar-screen").exists()).toBe(true);
+  });
 });
